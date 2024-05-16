b4edc866c0c999526a987e02f96d280c2d677727
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2024-05-16 01:28:48 -0300

    Remoção de comentários.

--- index.py
+++ index.py
@@ -1,21 +1,18 @@
 import openai
 
-# Insira sua chave de API do OpenAI aqui
-api_key = "sua_chave_de_api"
+api_key = "chaveapi"
 
-# Configuração da chave de API
 openai.api_key = api_key
 
-# Função para obter uma resposta do ChatGPT
 def obter_resposta(prompt):
     try:
         response = openai.Completion.create(
-            engine="text-davinci-002",  # Escolha o modelo do ChatGPT
+            engine="text-davinci-002", 
             prompt=prompt,
-            temperature=0.7,  # Controla a criatividade das respostas
-            max_tokens=150,   # Tamanho máximo da resposta
-            n=1,              # Quantidade de respostas retornadas
-            stop=None         # Condição de parada para a geração de texto
+            temperature=0.7,  
+            max_tokens=150,  
+            n=1,              
+            stop=None         
         )
         return response.choices[0].text.strip()
     except Exception as e:
